936155d54f985c13b41a558abf8bea9452401908
Yeglistar <yeglimar.30123032@uru.edu>
2024-11-07 07:44:42 -0400

    feat: add firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyBTAZ2_UF0UzrSwp7EwXCUSwWMcTIZ7Thk",
+      authDomain: "visualizador-f7aa8.firebaseapp.com",
+      projectId: "visualizador-f7aa8",
+      storageBucket: "visualizador-f7aa8.firebasestorage.app",
+      messagingSenderId: "442457913881",
+      appId: "1:442457913881:web:ac194177114ac3ed090888",
+    }
+  };
+  
